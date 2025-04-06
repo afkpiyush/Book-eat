b84fc4d139cce312de9169ab82f6ca9c92f4b592
afkpiyush <piyush.patil24@vit.edu>
2025-04-06 16:14:55 +0530

    Delete canteen.js

--- canteen.js
+++
@@ -1,6 +0,0 @@
-function goToMenu(canteenName) {
-    // Redirect to respective menu page
-    // You can name the pages: vit-menu.html, fruit-menu.html, etc.
-    window.location.href = `${canteenName}-menu.html`;
-  }
-  
